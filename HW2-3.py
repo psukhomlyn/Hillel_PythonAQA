0095ccf2d7006ff4e57b55a12102147319668f9e
psukhomlyn <petrsukhomlin@gmail.com>
2021-06-23 12:43:47 +0300

    HW2. Task 3. Blacklists

--- HW2-3.py
+++ HW2-3.py
@@ -3,6 +3,9 @@
 Имена людей в формате "John Dow" первое и второе имя. Найти тех кто состоит во всех черных списках.
 """
 
-bingo_blacklist
-poker_blacklist
-majong_blacklist
+bingo_blacklist = {'John Smith', 'Adam Silver', 'Erik Dier', 'Marta Adams'}
+poker_blacklist = {'George Washignton', 'Marta Adams', 'Frank Smith', 'John Smith'}
+majong_blacklist = {'John Smith', 'Ben Aflek', 'Marta Adams', 'Marta Williams'}
+
+black_users = bingo_blacklist.intersection(poker_blacklist).intersection(majong_blacklist)
+print(f'People who are in all blacklists are: {black_users}')
