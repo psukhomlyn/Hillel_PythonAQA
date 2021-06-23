c28bbfb45cfbaeba8cac24c925eda3329473bbc3
psukhomlyn <petrsukhomlin@gmail.com>
2021-06-23 12:30:28 +0300

    Lesson 2. Home Task 3

---
+++ HW2-3.py
@@ -0,0 +1,8 @@
+"""
+У вас есть 3 группы людей bigno_blacklist, poker_blacklist, majong_blacklist.
+Имена людей в формате "John Dow" первое и второе имя. Найти тех кто состоит во всех черных списках.
+"""
+
+bingo_blacklist
+poker_blacklist
+majong_blacklist
