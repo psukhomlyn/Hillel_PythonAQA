2516da383dfc39359ffeb34d3d39e147cbd1b4c3
psukhomlyn <petrsukhomlin@gmail.com>
2021-06-25 12:32:07 +0300

    HW2. Task 5. Guests

--- HW2-5.py
+++ HW2-5.py
@@ -13,11 +13,14 @@ if guest_name == '':
 elif guest_name in vip:
     vip_seat = vip.index(guest_name) + 1  # becaues index start from 0, seats start from 1
     print(f"{guest_name}, your seat is {vip_seat} in VIP zone")
+elif guest_name in common:
+    common_seat = common.index(guest_name) + 1
+    print(f"{guest_name}, your seat is {common_seat} in common zone")
 else:
     common.insert(common.index(""), guest_name)
     common.remove("")
     common_seat = common.index(guest_name) + 1
-    print(f"{guest_name}, your seat is {common_seat} in common zone")
+    print(f"{guest_name}, sorry, we cannot offer you seat in VIP zone, only in common zone. Your seat in common zone is {common_seat}")
 
 print(f'Guests list in vip zone is: {vip}')
 print(f'Guests list in common zone is: {common}')
