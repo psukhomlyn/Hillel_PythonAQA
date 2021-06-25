87ce460d9c0da3b3b8af68b8b980e72ea77a2688
psukhomlyn <petrsukhomlin@gmail.com>
2021-06-25 12:25:42 +0300

    HW2. Task 5. Guests

--- HW2-5.py
+++ HW2-5.py
@@ -3,3 +3,21 @@
 Есть группа гостей в общем зале и места в нем все еще есть. Отобразите 2 группы гостей в коде.
 """
 
+vip = ("John Snow", "Dina Watsons", "", "Karl Edwards")
+common = ["Din Ambrose", "", "Oleg Vasin", "", "Anna Smith"]
+
+guest_name = input("What is your name? ")
+
+if guest_name == '':
+    raise(ValueError)
+elif guest_name in vip:
+    vip_seat = vip.index(guest_name) + 1  # becaues index start from 0, seats start from 1
+    print(f"{guest_name}, your seat is {vip_seat} in VIP zone")
+else:
+    common.insert(common.index(""), guest_name)
+    common.remove("")
+    common_seat = common.index(guest_name) + 1
+    print(f"{guest_name}, your seat is {common_seat} in common zone")
+
+print(f'Guests list in vip zone is: {vip}')
+print(f'Guests list in common zone is: {common}')
