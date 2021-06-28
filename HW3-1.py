c6cf1868e388cce2a27d60daf613a5b2b299b221
psukhomlyn <petrsukhomlin@gmail.com>
2021-06-28 13:50:48 +0300

    HW3. Task 1. Add For loop

--- HW3-1.py
+++ HW3-1.py
@@ -7,17 +7,24 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 even_index = []
 odd_index = []
-for digit in numbers:
-    if numbers.index(digit) % 2 == 0:
-        even_index.append((numbers.index(digit), digit))
+
+# for digit in numbers:  #for each loop
+#     if numbers.index(digit) % 2 == 0:
+#         even_index.append((numbers.index(digit), digit))
+#     else:
+#         odd_index.append((numbers.index(digit), digit))
+#
+# print(f'Elements on even indexes are: {even_index}')
+# print(f'Elements on odd indexes are: {odd_index}')
+
+for index in range(len(numbers)):  #for loop
+    if index % 2 == 0:
+        even_index.append((index, numbers[index]))
     else:
-        odd_index.append((numbers.index(digit), digit))
+        odd_index.append((index, numbers[index]))
 
 print(f'Elements on even indexes are: {even_index}')
 print(f'Elements on odd indexes are: {odd_index}')
-
-
-
 
 
 
