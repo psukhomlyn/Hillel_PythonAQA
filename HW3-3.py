07ce6eaa4fa69655cda9a466c3c348bba2e2fa9f
psukhomlyn <petrsukhomlin@gmail.com>
2021-06-28 13:29:31 +0300

    HW3. Task 3. Friends vs Enemies

---
+++ HW3-3.py
@@ -0,0 +1,25 @@
+"""
+У вас есть 2 списка имен friends = ["John", "Marta", "James"] и enamies = ["John", "Johnatan", "Artur"].
+Перебрать имена друзей и написать сообщение f"{friend} we are the best friends" если друг не оказался в списке имен врагов.
+И вывести сообщение f"{friend} we are not the friends anymore" если друг оказался в списке врагов.
+Если имя друга James не проверяем его ибо он лучшый друг.
+"""
+
+friends = ["John", "Marta", "James"]
+enemies = ["John", "Johnatan", "Artur"]
+
+# for friend in friends:
+#     if friend not in enemies:
+#         print(f"{friend} we are the best friends")
+#     elif friend in enemies:
+#         print(f"{friend} we are not the friends anymore")
+#     else:
+#         print('James is the best')
+
+for friend in friends:
+    if friend == "James":
+        continue  # for skip check for James. James should not be checked according to task description.
+    elif friend not in enemies:
+        print(f"{friend} we are the best friends")
+    else:
+        print(f"{friend} we are not the friends anymore")
