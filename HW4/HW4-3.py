9e68dc13e8d1815c4ef74208108d517598efd65f
psukhomlyn <petrsukhomlin@gmail.com>
2021-07-05 16:18:34 +0300

    HW4. Task 3. Query

--- HW4/HW4-3.py
+++ HW4/HW4-3.py
@@ -2,3 +2,12 @@
 3 есть строка переданная в качестве квери параметров "     name=Amanda=sssss&age=32&&salary=1500&currency=quro                             ".
 Преобразовать эту строку в словарь где ключем должно быть значение перед = а значение пары значение после равно {name: Amanda......}
 """
+
+query = "     name=Amanda=sssss&age=32&&salary=1500&currency=quro                             "
+query_list = []
+query_dict = {}
+query_pairs = query.strip().split('&')
+for pair in query_pairs:
+    pair.split('=', maxsplit=1)
+    print(pair)
+print(query_dict)
