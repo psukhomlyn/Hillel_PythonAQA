21cd8cee1a98d5f40a2977e3de9e43728cfc7fa0
psukhomlyn <petrsukhomlin@gmail.com>
2021-07-05 11:56:51 +0300

    HW4. Task 1. Names Title

--- HW4/HW4-1.py
+++ HW4/HW4-1.py
@@ -2,7 +2,12 @@
 1 есть строка с именами разделенная пробелами "john marta james Morgan Adam Maria huang".
 Преобразовать строку таким образом что бы каждое имя однозначно начиналось с большой буквы.
 """
-
 names = "john marta james Morgan Adam Maria huang"
+title_names = []
 
-print(names.upper())
+names_list = names.split(" ")
+for name in names_list:
+    name = name.title()
+    title_names.append(name)
+title_names_str = ' '.join(title_names)
+print(f"Converted names string is: {title_names_str}")
