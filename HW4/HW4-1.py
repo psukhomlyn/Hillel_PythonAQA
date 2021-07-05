939de35fe18135d14731e80a3d6e22e600215fc4
psukhomlyn <petrsukhomlin@gmail.com>
2021-07-05 09:03:04 +0300

    Folders structure

---
+++ HW4/HW4-1.py
@@ -0,0 +1,8 @@
+"""
+1 есть строка с именами разделенная пробелами "john marta james Morgan Adam Maria huang".
+Преобразовать строку таким образом что бы каждое имя однозначно начиналось с большой буквы.
+"""
+
+names = "john marta james Morgan Adam Maria huang"
+
+print(names.upper())
---
+++ HW4/HW4-2stars.py
@@ -0,0 +1,4 @@
+"""
+** у вас есть тот же файл с разметкой страницы но нужно выдать структуру в которое не вся ссылка а лишь доменное имя:
+[('google', 'google.com', 'Google'), ('facebook', 'facebook.com', 'Facebook'), ('amazon', 'amazon.com', 'Amazon')]
+"""
