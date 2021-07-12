e6ba6aeefa4ba21e0f0737f989965c2a15a930aa
psukhomlyn <petrsukhomlin@gmail.com>
2021-07-12 14:55:21 +0300

    HW 5. Task 2. Read file and operations

--- HW5/HW5-2.py
+++ HW5/HW5-2.py
@@ -3,3 +3,18 @@
 Результат операции вывести в консоль.
 Использовать импорты для импортирования из модуля первой задачи нельзя.
 """
+result = 0
+with open("test/data/tuples_list.txt", mode="r") as file:
+    file_data = file.read()
+    #print(file_data.split('\n'))
+rows_list = file_data.split('\n')
+for row in rows_list:
+    row = row.replace('"', '').split("  ")
+    for element in row:
+        if int(row[2]) == 1:
+             result = int(row[0]) + int(row[1])
+        if int(row[2]) == 2:
+             result = int(row[0]) - int(row[1])
+        if int(row[2]) == 3:
+             result = int(row[0]) * int(row[1])
+    print(result)
