c66ea0313c0c124389cbd6058e7ebb02cac1a544
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-07-13 23:13:53 +0300

    HW5: add comments for each task with description of issues and list of warnings for resolve

--- HW5/HW5-1.py
+++ HW5/HW5-1.py
@@ -19,4 +19,17 @@ with open("test/data/tuples_list.txt", mode="w") as file:
         file.write(str(my_tuple).replace(')', '"\n').replace(',', ' ').replace('(', '"'))
 
 
+# Good but we don't know anything about comprehensions yet) -1 point
+# Too complicated and value should be in next format in file in each line:
+# left_operand right_operand operation\n
+# -2 points - for not correct solution
+# -1 point for dirty code
+# -1 point for file in repository
+
+# TODO: fix next warnings:
+#   HW5-1.py:2:80: E501 line too long (89 > 79 characters)
+#   HW5-1.py:9:80: E501 line too long (88 > 79 characters)
+#   HW5-1.py:10:80: E501 line too long (131 > 79 characters)
+#   HW5-1.py:18:80: E501 line too long (97 > 79 characters)
+#   HW5-1.py:19:80: E501 line too long (89 > 79 characters)
 
--- HW5/HW5-2.py
+++ HW5/HW5-2.py
@@ -3,7 +3,7 @@
 Результат операции вывести в консоль.
 Использовать импорты для импортирования из модуля первой задачи нельзя.
 """
-result = 0
+result = 0  # i am not really sure why this variable is needed
 with open("test/data/tuples_list.txt", mode="r") as file:
     file_data = file.read()
     #print(file_data.split('\n'))
@@ -19,3 +19,28 @@ for row in rows_list:
     if int(row_edited[2]) == 3:
         result = int(row_edited[0]) * int(row_edited[1])
     print(result)
+
+# not bad but it could be written easier. Take a look on pickle module which
+# I have show on lesson 5
+# -1 point fro dirty code
+# TODO: rows_list - name is overheaded since we already know that it is list.
+#        I suggest to rename it on lines or rows.
+# TODO: this block could be simplefied
+#     if int(row_edited[2]) == 1:
+#         result = int(row_edited[0]) + int(row_edited[1])
+#     if int(row_edited[2]) == 2:
+#         result = int(row_edited[0]) - int(row_edited[1])
+#     if int(row_edited[2]) == 3:
+#         result = int(row_edited[0]) * int(row_edited[1])
+#         ------------------------------------------
+#     left, right, operator = row_edited
+#     if operator == '1':
+#         print(int(left_operand) + int(right_operand))
+#     elif operator == '2':
+#         print(int(left_operand) - int(right_operand))
+#     elif operator == '3':
+#         print(int(left_operand) * int(right_operand))
+# It is look cleaner then before but better will be to use pickle module
+# TODO: resolve warning below^
+#  HW5-2.py:2:80: E501 line too long (109 > 79 characters)
+#  HW5-2.py:9:5: E265 block comment should start with '# '
