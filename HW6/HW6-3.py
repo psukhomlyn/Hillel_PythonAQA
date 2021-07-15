12fb5c69d41f52218f98e40662493e6a9026b5aa
psukhomlyn <petrsukhomlin@gmail.com>
2021-07-15 15:37:02 +0300

    HW 6. Task 3. Prime number

--- HW6/HW6-3.py
+++ HW6/HW6-3.py
@@ -8,12 +8,10 @@ def is_prime(x):
     if x < 2 or x > 1000:
         return f'Number is out of range from 2 to 1000'
     else:
-        if x <= 1 or x % 1 > 0:
-            return f'Entered number is not prime'
         for i in range(2, x):
             if x % i == 0:
                 return f'Entered number is not prime'
         return f'Entered number is prime'
 
 
-print(is_prime(71))
+print(is_prime(7))
